bb62d726aee0ba563d664e4dadddca2b536dcf95
rosemschneider <rosemschneider@gmail.com>
2020-07-20 16:37:25 -0700

    debugging

--- www/js/Trial.js
+++ www/js/Trial.js
@@ -102,11 +102,11 @@ function GiveN(SubjID, KL, Ans, AskNumber, Params, KnowerLevelResult, type, nonT
     Params.CurrTrial = Params.CurrTrial + 1;
 
     //Make an array for tracking NCorrect, etc. 
-	Params.Tracker = [];
+	Params.Tracker[Params.CurrTrial-1] = [];
 
-	for (var i = 1; i <= HighestTestNumber; i++) {
-		Params.Tracker[i-1] = [i, 0, 0, 0, 0]; //[N, NTrials, NCorrect, NInc, NFalse]
-	}
+	// for (var i = 1; i <= HighestTestNumber; i++) {
+	// 	Params.Tracker[i-1] = [i, 0, 0, 0, 0]; //[N, NTrials, NCorrect, NInc, NFalse]
+	// }
 
 
     //This loops goes through each number from 1 to the highest number

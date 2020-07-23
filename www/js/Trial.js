786de9faa62d26ac0e938c35883d114e9315bbf3
rosemschneider <rosemschneider@gmail.com>
2020-07-23 13:55:02 -0700

    uncontrolled check

--- www/js/Trial.js
+++ www/js/Trial.js
@@ -287,6 +287,14 @@ function GiveN(SubjID, KL, Ans, AskNumber, Params, KnowerLevelResult, type, nonT
 								AskNumber = Params.maxNumber -1;
 								maxNumberInit = 0;
 							}
+						} else if (AskNumber < Params.maxNumber) {
+							if (AskNumber + 1 < Params.maxNumber) {
+								AskNumber = AskNumber + 1;
+								maxNumberInit = 0;
+							} else {
+								AskNumber = Params.maxNumber -1;
+								maxNumberInit = 0;
+							}
 						} 
 					} else {
 						if (AskNumber != 1) {

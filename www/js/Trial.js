5b70dd50316521bf90b8406944289cd6b69fae8b
rosemschneider <rosemschneider@gmail.com>
2020-08-03 15:59:29 -0700

    checking false response

--- www/js/Trial.js
+++ www/js/Trial.js
@@ -143,15 +143,15 @@ function GiveN(SubjID, KL, Ans, AskNumber, Params, KnowerLevelResult, type, nonT
 				//they do not know N
 				KLMatrix[Ans-1] = -1;
 			}
-			if (NumFalseAnswer == 1 && NumSuccessesAnswer >= 2 || NumFailuresAnswer >= 2) {
-				//this is to catch kids who had previously shown evidence of knowing N
-				//But then start giving N incorrectly for other numbers
-				//This will update KLMatrix to -1
-				//This takes into account both successes and failures
-				if(NumSuccessesAnswer/(NumSuccessesAnswer + NumFailuresAnswer + NumFalseAnswer) < 2/3) {
-					KLMatrix[Ans-1]= -1;
-				}
-			} 
+			// if (NumFalseAnswer == 1 && NumSuccessesAnswer >= 2 || NumFailuresAnswer >= 2) {
+			// 	//this is to catch kids who had previously shown evidence of knowing N
+			// 	//But then start giving N incorrectly for other numbers
+			// 	//This will update KLMatrix to -1
+			// 	//This takes into account both successes and failures
+			// 	if(NumSuccessesAnswer/(NumSuccessesAnswer + NumFailuresAnswer + NumFalseAnswer) < 2/3) {
+			// 		KLMatrix[Ans-1]= -1;
+			// 	}
+			// } 
 			if (NumFalseAnswer + NumFailuresAnswer >= 3 && NumSuccessesAnswer/(NumSuccessesAnswer + NumFailuresAnswer) < 2/3) {
 				KLMatrix[Ans-1] = -1;
 			}
